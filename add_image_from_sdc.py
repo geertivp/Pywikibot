fcf65acf258da747648f227dda530989629242f9
Geert Van Pamel <geertivp@gmail.com>
2022-12-24 10:37:42 +0100

    Improved media file assignment algorithm
    Ignore category instance items.
    Ignore "limited to" depict items.
    Get image subtypes (grave, interior, logo) as documented by "normal" depict items.

--- add_image_from_sdc.py
+++ add_image_from_sdc.py
@@ -3,22 +3,33 @@
 codedoc = """
 Add media files to Wikidata items
 from Wikimedia Commons SDC depicts (P180) statements,
-based on a Wikimedia Commons category.
+based on a Wikimedia Commons category (parameter P1),
+or a list of media files (stdin).
 
 Kind of creating a reverse SDC P180 statement in Wikidata...
 
 Starting from a Wikimedia Category,
+or a list of media files,
 trough Wikimedia Commons SDC P180 statements,
 registering media file statements (P10, P18, P51)
-on the corresponding Wikidata items.
+to the corresponding Wikidata items.
 
 Basically, it tries to do the same as WDFIST (Magnus Manske),
 on the base of a Wikimedia Commons category;
 a functionality that WDFIST does not offer…
 
+In prnciple the script runs completely automated, without any human intervention,
+but it is strongly advised to verify the resulting changes in Wikidata.
+Depending on the quality of the SDC P180 statements manual corrections might be required.
+Wikimedia Commons is not updated.
+
 Parameters:
 
     P1: Wikimedia Commons category (subcatergories are processed recursively)
+    
+    If no parameters are available,
+    a list of media filenames is read via stdin;
+    one filename per line.
 
 Options:
 
@@ -46,11 +57,13 @@ Prerequisites:
         can be proactively generated with the ISA Tool, as part of a campaign
         might be done with some Toolforge tool (unexisting yet?)
         manually adding depicts statements
+    If there are no SDC P180 statements, no updates in Wikidata are performed.
 
 Algorithm:
 
     List all files in the Wikimedia Commons category (recursively)
-    Verify if there is any SDC data linked to the media file
+    Or the list of files is read from stdin.
+    Verify if there is any SDC data linked to the media files
     Obtain any SDC P180 statement (depicts)
     Skip the media file if it is already used on wikidata
     Determine the media type
@@ -63,24 +76,33 @@ Algorithm:
 
 Error messages:
 
-    Add media file:                 Registering a new media file
-    File is used by:                Do not register the same media file multiple times
+    Add media file:                 Registering a new media file.
+    File is used by:                Do not register the same media file multiple times.
     Media belongs to collection:    Skipping GLAM collections that often depicts art work parts;
-                                    without a preferred qualifier there is a risk for wrong registrations
-    No depicts for file:            No depict statements found; no item number available
+                                    without a preferred qualifier there is a risk for wrong registrations.
+    No depicts for file:            No depict statements found; no item number available.
                                     We should encorage to add depicts statements;
-                                    at least one with preferred status
-    No statements for file:         Only labels; no statements, so no depicts
+                                    at least one with preferred status.
+    No statements for file:         Only labels; no statements, so no depicts.
     Redundant media file:           All media slots already taken, avoid having multiple media files;
-                                    maybe add more depicts statements
-    Skipping page:                  Page doesn't belong to the File namespace
+                                    maybe add more depicts statements to find more items.
+    Skipping page:                  Page doesn't belong to the File namespace.
+
+Tips and tricks:
+
+    Make a list of user uploads via AutoWikiBrowser
+    Paste the list of media files via stdin
 
 Known problems:
 
     Updated items might require manual validation, to correct any anomalies;
     see https://www.wikidata.org/wiki/Special:Contributions
     
-    Redirects are not retroactively updated in SDC statements
+    Category redirects are not traversed.
+    
+    Wikidata redirects are not retroactively updated in SDC statements.
+    
+    Wikidata item page was deleted while SDC P180 was still there.
 
     "Wrong media file" assigned to the item:
         Caused by a wrong SDC P180 statement
@@ -109,6 +131,11 @@ Known problems:
 
     Multiple timeout problems... could we set a hard timeout?
 
+    Other properties; image could be one off:
+        P154    logo
+        P1442   grave
+        P5775   interior
+
 Documentation:
 
     https://doc.wikimedia.org/pywikibot/master/api_ref/pywikibot.html
@@ -116,9 +143,10 @@ Documentation:
     https://buildmedia.readthedocs.org/media/pdf/pywikibot/stable/pywikibot.pdf
 
     https://byabbe.se/2020/09/15/writing-structured-data-on-commons-with-python
-        Prototype SDC queries
+    Prototype SDC queries
 
     https://be.wikimedia.org/wiki/ISA_Tool
+    Tool to generate SDC P180 depict statements and captions (SDC labels).
 
 Related projects:
 
@@ -137,6 +165,7 @@ import pywikibot		# API interface to Wikidata
 import sys		    	# System: argv, exit (get the parameters, terminate the program)
 from pywikibot import pagegenerators as pg
 
+# Constants
 MAINNAMESPACE = 0
 FILENAMESPACE = 6
 
@@ -146,11 +175,15 @@ INSTANCEPROP = 'P31'
 AUTHORPROP = 'P50'
 AUDIOPROP = 'P51'
 PUBLISHERPROP = 'P123'
+LOGOPROP = 'P154'
 DEPICTSPROP = 'P180'
 COLLECTIONPROP = 'P195'
 SUBCLASSPROP = 'P279'
 MIMEPROP = 'P1163'
+GRAVEPROP = 'P1442'
+INTERIORPROP = 'P5775'
 
+CATEGORYINSTANCE = 'Q4167836'
 DISAMBUGINSTANCE = 'Q4167410'
 
 # Global variables
@@ -160,13 +193,25 @@ pgmlic = 'MIT License'
 creator = 'User:Geertivp'
 transcmt = '#pwb Add image from SDC'
 
-# Mapping of media types to property
-# Should be amended for new types
+# Map image instance to property
+image_types = {
+    'Q173387': 'grave',
+    'Q1886349': 'logo',
+    'Q31807746': 'interior',
+    # others...
+}
+
+# Mapping of SDC media MIME types to Wikidata property
+# Should be extended for new MIME types -> will generate a KeyError
 media_props = {
-    #'application': '',     # Not implemented
+    #'application': '',     # Not implemented; generates error
     'audio': AUDIOPROP,
+    'grave': GRAVEPROP,
     'image': IMAGEPROP,
+    'interior': INTERIORPROP,
+    'logo': LOGOPROP,
     'video': VIDEOPROP,
+    # others...
 }
 
 mainlang = os.getenv('LANG', 'en').split('_')[0]      # Default description language
@@ -174,23 +219,39 @@ mainlang = os.getenv('LANG', 'en').split('_')[0]      # Default description lang
 # Get parameters
 pgmnm = sys.argv.pop(0)
 
-category = sys.argv.pop(0)
+pywikibot.info('{}, {}, {}, {}'.format(pgmnm, pgmid, pgmlic, creator))
 site = pywikibot.Site('commons')
 repo = site.data_repository()
-cat = pywikibot.Category(site, category)
 
-pywikibot.info('{}, {}, {}, {}'.format(pgmnm, pgmid, pgmlic, creator))
-pywikibot.info(cat.categoryinfo)
+# Get input parameters (either P1 or stdin)
+if sys.argv:
+    # Get Wikimedia Commons page list from category P1
+    category = sys.argv.pop(0)
 
-# Get recursive image list from category
-for page in pg.CategorizedPageGenerator(cat, recurse = True):
-    # We only accept the File: namespace
-    media_identifier = 'M' + str(page.pageid)
-    if page.namespace() != FILENAMESPACE:
-        pywikibot.log('Skipping {} {}'.format(media_identifier, page.title()))
-        continue
+    # Get recursive image list from category
+    cat_list = pywikibot.Category(site, category)
+    pywikibot.info('Category:{}: {}'.format(category, cat_list.categoryinfo))
+    page_list = pg.CategorizedPageGenerator(cat_list, recurse = True)
+else:
+    # Read Wikimedia Commons page list from stdin
+    inputfile = sys.stdin.read()
+    input_list = sorted(set(inputfile.split('\n')))
+    page_list = []
+    for subject in input_list:
+        if subject:
+            try:
+                page_list.append(pywikibot.FilePage(site, subject))
+            except Exception as error:
+                pywikibot.log(error)
 
+for page in page_list:
     try:
+        # We only accept the File: namespace
+        media_identifier = 'M' + str(page.pageid)
+        if page.namespace() != FILENAMESPACE:
+            pywikibot.log('Skipping {} {}'.format(media_identifier, page.title()))
+            continue
+
         # Get media SDC data
         request = site.simple_request(action='wbgetentities', ids=media_identifier)
         row = request.submit()
@@ -227,9 +288,12 @@ for page in pg.CategorizedPageGenerator(cat, recurse = True):
         itempage = pywikibot.FilePage(repo, page.title())
         image_used = False
 
+        # This includes P10 video, P18 image, P51 audio, etc.
+        # Possibly other links?
         for file_ref in pg.FileLinksGenerator(itempage):
-            # We only take Qnumbers into account (primary namespace)
             if file_ref.namespace() == MAINNAMESPACE:
+                # We only take Qnumbers into account (primary namespace)
+                # e.g. ignore descriptive pages
                 image_used = True
                 pywikibot.log('{} {} is used by {}'
                               .format(media_identifier, page.title(), file_ref.title()))
@@ -237,10 +301,19 @@ for page in pg.CategorizedPageGenerator(cat, recurse = True):
             # Image is already used, so skip
             continue
 
+        # Default file type; can be overruled by depict statement
+        file_type = ['image']
+        mime_list = statement_list.get(MIMEPROP)
+        if mime_list:
+            # We now have valid depicts statements, so we can obtain the media type
+            # Default: image
+            mime_type = mime_list[0]['mainsnak']['datavalue']['value']
+            file_type = mime_type.split('/')
+
         item_list = []
         for depict in depict_list:
             # Loop through the list of SDC P180 statements,
-            # in order of priority (ignore normal when at least one preferred)
+            # in order of priority
             """
 {'mainsnak':
 	{'snaktype': 'value', 'property': 'P180', 'hash': 'de0ee4f082bfc89cdb25db93cc21755315974690',
@@ -251,13 +324,22 @@ for page in pg.CategorizedPageGenerator(cat, recurse = True):
             """
             # Get the Q-number and item
             qnumber = depict['mainsnak']['datavalue']['value']['id']
+            if qnumber in image_types:
+                # Overrule preferred items, or else add the item to the list
+                file_type = [image_types[qnumber]]
 
-            # Overrule preferred items, or else add the item to the list
             if depict['rank'] == 'preferred':
+                # Ignore normal when at least one preferred
                 item_list = [qnumber]
-                break
-            else:
+            elif not ('qualifiers' in depict and 'P518' in depict['qualifiers']):
+                # Skip "aplies to" qualifiers
+                # Could also filter on minimum image resolution
+                # Add a normal ranked item to the list
                 item_list.append(qnumber)
+
+        # Could possibly fail with KeyError when non-recognized media types
+        # In that case the new media type should be added
+        media_type = media_props[file_type[0]]
 
         description_list = sdc_data.get('descriptions')
         if description_list != {}:
@@ -265,20 +347,8 @@ for page in pg.CategorizedPageGenerator(cat, recurse = True):
             # The GUI only allows to register labels?
             pywikibot.log(description_list)
 
-        file_type = ['image']
-        mime_list = statement_list.get(MIMEPROP)
-        if mime_list:
-            # We now have valid depicts statements, so we can obtain the media type
-            # Default: image
-            mime_type = mime_list[0]['mainsnak']['datavalue']['value']
-            file_type = mime_type.split('/')
-
-        # Could possibly fail with non-recognized media types
-        # In that case the new media type should be added
-        media_type = media_props[file_type[0]]
-
         for qnumber in item_list:
-            # Loop through the target Wikidata items
+            # Loop through the target Wikidata items to find a match
             item = pywikibot.ItemPage(repo, qnumber)
 
             try:
@@ -297,15 +367,14 @@ for page in pg.CategorizedPageGenerator(cat, recurse = True):
                 # Skip when neither instance, nor subclass
                 continue
             elif ((INSTANCEPROP in item.claims
-                        and item.claims[INSTANCEPROP][0].getTarget().getID() == DISAMBUGINSTANCE)
+                        and item.claims[INSTANCEPROP][0].getTarget().getID() in {CATEGORYINSTANCE, DISAMBUGINSTANCE})
                     or (AUTHORPROP in item.claims)
                     or (PUBLISHERPROP in item.claims)):
-                # Skip Wikimedia disambiguation items
+                # Skip Wikimedia disambiguation and category items
                 # See https://www.wikidata.org/wiki/Property:P18#P2303
-                # Note that we accept P279 subclass
+                # Note that we unditionally accept P279 subclass
                 # In general we skip publications
                 continue
-                # Could also filter on minimum image resolution
             elif media_type not in item.claims:
                 # Only add exclusive media file (preferably one single image per Wikidata item)
                 try:
