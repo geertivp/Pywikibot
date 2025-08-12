3f9e5afdfc99dc7614a83354a9cd5361149221b9
Geert Van Pamel <geertivp@gmail.com>
2025-08-12 12:30:29 +0200

    targetx and functions

--- create_item.py
+++ create_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 codedoc = """
-Create a new item.
+Create a new item
 
 Parameters:
 
@@ -59,17 +59,12 @@ pgmid = '2025-08-11 (gvp)'	    # Program ID and version
 pgmlic = 'MIT License'
 creator = 'User:Geertivp'
 
-"""
-    Static definitions
-"""
-
 # Technical configuration flags
+MAINLANG = 'en:mul'
+
 # Defaults: transparent and safe
-debug = False		# Can be activated with -d (errors and configuration changes are always shown)
-errorstat = True    # Show error statistics (disable with -e)
 exitfatal = True	# Exit on fatal error (can be disabled with -p; please take care)
 shell = True		# Shell available (command line parameters are available; automatically overruled by PAWS)
-verbose = True		# Can be set with -q or -v (better keep verbose to monitor the bot progress)
 
 # Technical parameters
 
@@ -114,6 +109,102 @@ def fatal_error(errcode, errtext):
         pywikibot.error('Proceed after fatal error')
 
 
+def get_item_header(header):
+    """Get the item header (label, description, alias in user language).
+
+    :param header: item label, description, or alias language list (string or list)
+    :return: label, description, or alias in the first available language (string)
+    """
+
+    # Return preferred label
+    for lang in main_languages:
+        if lang in header:
+            return header[lang]
+
+    # Return any other label
+    for lang in header:
+        return header[lang]
+    return '-'
+
+
+def get_property_label(propx) -> str:
+    """Get the label of a property.
+
+    :param propx: property (string or property)
+    :return property label (string)
+    Except: undefined property
+    """
+
+    if isinstance(propx, str):
+        propty = pywikibot.PropertyPage(repo, propx)
+    else:
+        propty = propx
+
+    return get_item_header(propty.labels)
+
+
+def get_item_page(qnumber) -> pywikibot.ItemPage:
+    """Get the item; handle redirects.
+    """
+    if isinstance(qnumber, str):
+        item = pywikibot.ItemPage(repo, qnumber)
+        try:
+            item.get()
+        except pywikibot.exceptions.IsRedirectPageError:
+            # Resolve a single redirect error
+            item = item.getRedirectTarget()
+            label = get_item_header(item.labels)
+            pywikibot.warning('Item {} ({}) redirects to {}'
+                              .format(label, qnumber, item.getID()))
+            qnumber = item.getID()
+    else:
+        item = qnumber
+        qnumber = item.getID()
+
+    while item.isRedirectPage():
+        ## Should fix the sitelinks
+        item = item.getRedirectTarget()
+        label = get_item_header(item.labels)
+        pywikibot.warning('Item {} ({}) redirects to {}'
+                          .format(label, qnumber, item.getID()))
+        qnumber = item.getID()
+
+    return item
+
+
+def get_language_preferences() -> []:
+    """Get the list of preferred languages,
+    using environment variables LANG, LC_ALL, and LANGUAGE.
+    'en' is always appended.
+
+    Format: string delimited by ':'.
+    Main_sublange code,
+
+    Result:
+
+        List of ISO 639-1 language codes
+    Documentation:
+
+        https://www.gnu.org/software/gettext/manual/html_node/Locale-Environment-Variables.html
+        https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+    """
+    mainlang = os.getenv('LANGUAGE',
+                         os.getenv('LC_ALL',
+                         os.getenv('LANG', MAINLANG))).split(':')
+    main_languages = [lang.split('_')[0] for lang in mainlang]
+
+    # Cleanup language list
+    for lang in main_languages:
+        if len(lang) > 3:
+            main_languages.remove(lang)
+
+    for lang in MAINLANG.split(':'):
+        if lang not in main_languages:
+            main_languages.append(lang)
+
+    return main_languages
+
+
 def wd_proc_all_items():
     """
     """
@@ -129,8 +220,7 @@ def wd_proc_all_items():
     errsleep = 0	    	# Technical error penalty (sleep delay in seconds)
 
 # Avoid that the user is waiting for a response while the data is being queried
-    if verbose:
-        print('\nProcessing statements')
+    pywikibot.info('\nProcessing statements')
 
 # Transaction timing
     now = datetime.now()	# Start the main transaction timer
@@ -146,7 +236,7 @@ def wd_proc_all_items():
         status = 'Skip'
         errcount += 1
         exitstat = max(exitstat, 3)
-        print('Bad name: %s' % (objectname), file=sys.stderr)
+        pywikibot.error('Bad name: {}'.format(objectname))
       elif objectname > "/":    # Skip bad labels
         status = 'OK'
         label = {}
@@ -166,22 +256,15 @@ def wd_proc_all_items():
             request = api.Request(site=repo, parameters=params)
             result = request.submit()
 
-            if debug:
-                print(result)
+            pywikibot.debug(result)
             if 'search' in result:
                 for row in result['search']:
-                    item = pywikibot.ItemPage(repo, row['id'])
-                    try:
-                        item.get()
-                    except pywikibot.exceptions.IsRedirectPageError:
-                        # Resolve a single redirect error
-                        item = item.getRedirectTarget()
-                        pywikibot.warning('Item {} redirects to {}'.format(row['id'], item.getID()))
+                    item = get_item_page(row['id'])
 
                     if INSTANCEPROP in item.claims:
                         for seq in item.claims[INSTANCEPROP]:       # Instance
                             instance = seq.getTarget()
-                            if instance.getID() == target[INSTANCEPROP]:
+                            if instance == targetx[INSTANCEPROP]:
                                 for lang in item.labels:
                                     if objectname == item.labels[lang]:##accent fallback
                                         status = 'Update'
@@ -242,10 +325,16 @@ def wd_proc_all_items():
                     propstatus = 'OK'
                     if propty in item.claims:
                         for seq in item.claims[propty]:
-                            val = seq.getTarget().getID()
-                            if val == target[propty]:
+                            val = seq.getTarget()
+                            if val == targetx[propty]:
                                 propstatus = 'Skip'
                                 break
+                            else:
+                                propstatus = 'Other'
+                                pywikibot.warning('Conflicting statement {}:{} ({}:{}) - {} ({}) for {}'
+                                                  .format(get_property_label(propty), get_item_header(targetx[propty].labels),
+                                                          propty, targetx[propty].getID(),
+                                                          get_item_header(val.labels), val.getID(), qnumber))
 
                     if propstatus == 'OK':      # Claim is missing, so add it now
                         claim = pywikibot.Claim(repo, propty)
@@ -290,23 +379,17 @@ def wd_proc_all_items():
         prevnow = now	        	# Transaction status reporting
         now = datetime.now()	    # Refresh the timestamp to time the following transaction
 
-        if verbose or status not in ['OK']:		# Print transaction results
-            isotime = now.strftime("%Y-%m-%d %H:%M:%S") # Only needed to format output
-            totsecs = (now - prevnow).total_seconds()	# Elapsed time for this transaction
-            print('%d\t%s\t%f\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % (transcount, isotime, totsecs, status, qnumber, objectname, commonscat, alias, nationality, descr[mainlang]))
+        isotime = now.strftime("%Y-%m-%d %H:%M:%S") # Only needed to format output
+        totsecs = (now - prevnow).total_seconds()	# Elapsed time for this transaction
+        pywikibot.info('%d\t%s\t%f\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % (transcount, isotime, totsecs, status, qnumber, objectname, commonscat, alias, nationality, descr[mainlang]))
 
 
 def show_help_text():
 # Show program help and exit (only show head text)
     helptxt = HELPRE.search(codedoc)
     if helptxt:
-        print(helptxt.group(0))	# Show helptext
+        pywikibot.info(helptxt.group(0))	# Show helptext
     sys.exit(1)         # Must stop
-
-
-def show_prog_version():
-# Show program version
-    pywikibot.info('{}, {}, {}, {}'.format(modnm, pgmid, pgmlic, creator))
 
 
 def get_next_param():
@@ -314,106 +397,75 @@ def get_next_param():
     Get the next command parameter, and handle any qualifiers
     """
 
-    global debug
-    global errwaitfactor
-    global exitfatal
-    global verbose
-
     cpar = sys.argv.pop(0)	    # Get next command parameter
-    if debug:
-        print('Parameter %s' % cpar)
 
-    if cpar.startswith('-d'):	# debug mode
-        debug = True
-        print('Setting debug mode')
-    elif cpar.startswith('-e'):	# error stat
-        errorstat = False
-        print('Disable error statistics')
-    elif cpar.startswith('-h'):	# help
+    if cpar.startswith('-h'):	# help
         show_help_text()
-    elif cpar.startswith('-m'):	# fast mode
-        errwaitfactor = 1
-        print('Setting fast mode')
-    elif cpar.startswith('-p'):	# proceed after fatal error
-        exitfatal = False
-        print('Setting proceed after fatal error')
-    elif cpar.startswith('-q'):	# quiet mode
-        verbose = False
-        print('Setting quiet mode')
-    elif cpar.startswith('-v'):	# verbose mode
-        verbose = True
-        print('Setting verbose mode')
-    elif cpar.startswith('-V'):	# Version
-        show_prog_version()
     elif cpar.startswith('-'):	# unrecognized qualifier (fatal error)
         fatal_error(4, 'Unrecognized qualifier; use -h for help')
     return cpar		# Return the parameter or the qualifier to the caller
 
 
-HELPRE = re.compile(r'^(.*\n)+\nDocumentation:\n\n(.+\n)+')  # Help text
-PROPRE = re.compile(r'P[0-9]+')             # P-number
-QSUFFRE = re.compile(r'Q[0-9]+')            # Q-number
-
-if verbose:
-    show_prog_version()	    	# Print the module name
-
-try:
-    pgmnm = sys.argv.pop(0)	    # Get the name of the executable
-    if debug:
-        pywikibot.info('{}, {}, {}, {}'.format(pgmnm, pgmid, pgmlic, creator))
-except:
-    shell = False
-    print('No shell available')	# Most probably running on PAWS Jupyter
-
 """
     Start main program logic
     Precompile the Regular expressions, once (for efficiency reasons; they will be used in loops)
 """
+HELPRE = re.compile(r'^(.*\n)+\nDocumentation:\n\n(.+\n)+')  # Help text
+PROPRE = re.compile(r'P[0-9]+')             # P-number
+QSUFFRE = re.compile(r'Q[0-9]+')            # Q-number
+
+try:
+    pgmnm = sys.argv.pop(0)	    # Get the name of the executable
+    pywikibot.info('{}, {}, {}, {}'.format(pgmnm, pgmid, pgmlic, creator))
+except:
+    shell = False
+    pywikibot.info('{}, {}, {}, {} (No shell available)'.format(modnm, pgmid, pgmlic, creator))
+
+# Get language list
+main_languages = get_language_preferences()
+mainlang = main_languages[0]
 
 inpar = '-'
 while len(sys.argv) > 0 and inpar.startswith('-'):
     inpar = get_next_param()
 
-# Get all claims from parameter list
-target={}
-targetx={}
+try:
+    # Connect to database
+    repo = pywikibot.Site('wikidata')  # Login to Wikibase instance
+    repo.login()            # Must login
+    wdbotflag = 'bot' in pywikibot.User(repo, repo.user()).groups()
 
-while len(sys.argv) > 1:
-    inpar = PROPRE.findall(inpar.upper())[0]
-    target[inpar] = QSUFFRE.findall(sys.argv.pop(0).upper())[0]
-    inpar = sys.argv.pop(0)
+    # Get all claims from parameter list
+    targetx = {}
 
-if not inpar.startswith('-'):
-    inpar = PROPRE.findall(inpar.upper())[0]
-    target[inpar] = QSUFFRE.findall(sys.argv.pop(0).upper())[0]
+    while len(sys.argv) > 1:
+        inpar = PROPRE.findall(inpar.upper())[0]
+        targetx[inpar] = get_item_page(QSUFFRE.findall(sys.argv.pop(0).upper())[0])
+        inpar = sys.argv.pop(0)
 
-if INSTANCEPROP not in target:
-    fatal_error(3, 'Missing {}'.format(INSTANCEPROP))
+    if not inpar.startswith('-'):
+        inpar = PROPRE.findall(inpar.upper())[0]
+        targetx[inpar] = get_item_page(QSUFFRE.findall(sys.argv.pop(0).upper())[0])
 
-# Global parameters
+except Exception as error:
+    # Other exception to be used
+    fatal_error(20, 'Data error, {}'.format(error))
 
-# Default description language
-mainlang = os.getenv('LANGUAGE',
-                     os.getenv('LC_ALL',
-                     os.getenv('LANG', 'en'))).split(':')[0].split('_')[0]
+# List the statements
+for propty in targetx:
+    proptyx = pywikibot.PropertyPage(repo, propty)
+    pywikibot.info('Statement {}:{} ({}:{})'
+                   .format(get_property_label(proptyx), get_item_header(targetx[propty].labels),
+                           propty, targetx[propty].getID()))
+
+if INSTANCEPROP not in targetx:
+    fatal_error(3, 'Missing {} ({})'.format(get_property_label(INSTANCEPROP), INSTANCEPROP))
 
 # Print preferences
 pywikibot.log('Main language:\t%s' % mainlang)
 pywikibot.log('Maximum delay:\t%d s' % maxdelay)
-pywikibot.log('Verbose mode:\t%s' % verbose)
-pywikibot.log('Debug mode:\t%s' % debug)
 pywikibot.log('Exit on fatal error:\t%s' % exitfatal)
 pywikibot.log('Error wait factor:\t%d' % errwaitfactor)
-
-# Connect to database
-repo = pywikibot.Site('wikidata')  # Login to Wikibase instance
-repo.login()            # Must login
-wdbotflag = 'bot' in pywikibot.User(repo, repo.user()).groups()
-
-for propty in target:
-    proptyx = pywikibot.PropertyPage(repo, propty)
-    targetx[propty] = pywikibot.ItemPage(repo, target[propty])
-    print('Add %s (%s): %s (%s)' % (proptyx.labels[mainlang], propty, targetx[propty].labels[mainlang], target[propty]), file=sys.stderr)
 
 # Get list of item numbers
 inputfile = sys.stdin.read()
@@ -421,8 +473,7 @@ itemlist = inputfile.splitlines()
 descr = {mainlang: itemlist.pop(0)}
 itemlist = sorted(set(itemlist))
 
-if debug:
-    print(itemlist)
+pywikibot.debug(itemlist)
 
 wd_proc_all_items()	# Execute all items for one language
 
@@ -431,9 +482,9 @@ wd_proc_all_items()	# Execute all items for one language
     PAWS is using tokens (passwords can't be used because Python scripts are public)
     Shell is using passwords (from user-password.py file)
 """
-if debug:
-    for site in sorted(pywikibot._sites.values()):
-        if site.username():
-            print(site, site.username(), site.is_oauth_token_available(), site.logged_in())
+for site in sorted(pywikibot._sites.values()):
+    if site.username():
+        pywikibot.debug('{} {} {} {}'.format(site, site.username(), site.is_oauth_token_available(), site.logged_in()))
 
 sys.exit(exitstat)
+
